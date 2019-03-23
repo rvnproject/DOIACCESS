f9856aa4c8f8192cf677157522dbf6273d0d7c39
mde-pach <12986913+mde-pach@users.noreply.github.com>
2019-03-23 18:32:25 +0100

    first commit with basic RavenDOI app

---
+++ src/options_custom/i18n.js
@@ -0,0 +1,51 @@
+// SAMPLE
+this.i18n = {
+    "settings": {
+        "en": "Settings",
+        "de": "Optionen"
+    },
+    "search": {
+        "en": "Search",
+        "de": "Suche"
+    },
+    "nothing-found": {
+        "en": "No matches were found.",
+        "de": "Keine Übereinstimmungen gefunden."
+    },
+    "enable": {
+        "en": "Enable",
+        "de": "Aktivieren"
+    },
+    "disconnect": {
+        "en": "Disconnect:",
+        "de": "Trennen:"
+    },
+    "about": {
+        "en": "About RavenDOI",
+        "fr": "À propos de RavenDOI"
+    },
+    "creators": {
+        "en": "Creators",
+        "fr": "Créateurs"
+    },
+    "creators_description": {
+        "en": "Based on an original concept of Paul Brochard and made with <i class=\"fa fa-coffee\" title=\"coffee\"></i> and <i class=\"fa fa-heart\" title=\"heart\"></i>  by Maxime de Pachtere.",
+        "fr": "Basé sur un concept original de Paul Brochard et conçu avec du <i class=\"fa fa-coffee\" title=\"coffee\"></i> et <i class=\"fa fa-heart\" title=\"heart\"></i> par Maxime de Pachtere."
+    },
+    "support_us": {
+        "en": "Support us",
+        "fr": "Soutenez nous"
+    },
+    "support_us_description" : {
+        "en": "This Chrome extension is completely free and open source. But if you want to support us, you can make <a href=\"https://paypal.me/mdepachtere\">a donation on paypal</a>.",
+        "fr": "Cette extension Chrome est entierement gratuite et open source. Neanmoins, si vous voulez nous soutenir, vous pouvez faire <a href=\"https://paypal.me/mdepachtere\">un don sur paypal</a>."
+    },
+    "credits": {
+        "en": "Credits",
+        "fr": "Crédits"
+    },
+    "credits_description": {
+        "en": "<a target=\"_blank\" rel=\"noopener noreferrer\" href=\"https://github.com/tpayet\">Thomas Payet</a> as Frontend mentor<br/>Logo \"Raven\" by Myly from <a target=\"_blank\" rel=\"noopener noreferrer\" href=\"https://thenounproject.com/\">the Noun Project</a>.<br/>Created with <a target=\"_blank\" rel=\"noopener noreferrer\" href=\"https://extensionizr.com\">extensionizr<a/>.",
+        "fr": "<a target=\"_blank\" rel=\"noopener noreferrer\" href=\"https://github.com/tpayet\">Thomas Payet</a> en tant que mentor Frontend<br/>Logo \"Raven\" par Myly depuis <a target=\"_blank\" rel=\"noopener noreferrer\" href=\"https://thenounproject.com/\">the Noun Project</a>. Creer avec <a target=\"_blank\" rel=\"noopener noreferrer\" href=\"https://extensionizr.com\">extensionizr<a/>."
+    }
+};
---
+++ src/options_custom/manifest.js
@@ -0,0 +1,30 @@
+// SAMPLE
+this.manifest = {
+    "name": "RavenDOI",
+    "icon": "../../images/icons/raven_icon_48.png",
+    "settings": [
+        {
+            "tab": i18n.get("about"),
+            "group": i18n.get("creators"),
+            "name": "creators",
+            "type": "description",
+            "text": i18n.get("creators_description")
+        },
+        {
+            "tab": i18n.get("about"),
+            "group": i18n.get("support_us"),
+            "name": "support_us",
+            "type": "description",
+            "text": i18n.get("support_us_description")
+        },
+        {
+            "tab": i18n.get("about"),
+            "group": i18n.get("credits"),
+            "name": "credits",
+            "type": "description",
+            "text": i18n.get("credits_description")
+        },
+    ],
+    "alignment": [
+    ]
+};
