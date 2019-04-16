d9125b6bf400b0d325b580ce07020ce4f5074ff9
mde-pach <12986913+mde-pach@users.noreply.github.com>
2019-04-16 10:50:59 +0200

    option page and translation

--- src/scripts/options.js
+++ src/scripts/options.js
@@ -1,30 +1,6 @@
 import ext from "./utils/ext";
-import storage from "./utils/storage";
+import internationalize from "./utils/custom_i18n"
 
-var colorSelectors = document.querySelectorAll(".js-radio");
-
-var setColor = (color) => {
-  document.body.style.backgroundColor = color;
-};
-
-storage.get('color', function(resp) {
-  var color = resp.color;
-  var option;
-  if(color) {
-    option = document.querySelector(`.js-radio.${color}`);
-    setColor(color);
-  } else {
-    option = colorSelectors[0]
-  }
-
-  option.setAttribute("checked", "checked");
-});
-
-colorSelectors.forEach(function(el) {
-  el.addEventListener("click", function(e) {
-    var value = this.value;
-    storage.set({ color: value }, function() {
-      setColor(value);
-    });
-  })
-})
+window.onload = function() {
+  internationalize();
+}
